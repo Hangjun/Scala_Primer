0984703914a9364d67b702aa838ad7d6e385ea63
Hangjun <ajxuhangjun2007@gmail.com>
2016-05-16 09:56:11 -0700

    Update 2.1_quickSort.scala

--- 2.1_quickSort.scala
+++ 2.1_quickSort.scala
@@ -1,6 +1,6 @@
 /* Two versions of quick sort in Scala */
 
-/* Version #1: Imperative programming */
+/* Version #1: Imperative Programming */
 def sort(xs: Array[Int]) {
 	def swap(i: Int, j: Int) {
 		val t: Int = xs(i); xs(i) = xs(j); xs(j) = t	
@@ -8,10 +8,10 @@ def sort(xs: Array[Int]) {
 
 	def sortHelper(left: Int, right: Int) {
 		val pivot = xs(left + (right - left)/2)
-		val i = left; val j = right;
+		val i = left; val j = right
 		while (i <= j) {
 			while (xs(i) < pivot) i += 1
-			while (xs(j) > pivot) J-= 1
+			while (xs(j) > pivot) J -= 1
 			if (i <= j) {
 				swap(i. j)
 				i += 1
@@ -25,3 +25,11 @@ def sort(xs: Array[Int]) {
 
 	sortHelper(0, xs.length - 1)
 }
+
+/* Version #2: Functional Programming */
+def sort(xs: Array[Int]) {
+	if (xs.length <= 1) xs
+	val pivot = xs(xs.length / 2)
+	Array.concat(sort(xs filter (pivot > )). xs filter (pivot == ). sort(xs filter (pivot < )))
+}
+
